4a3fb0e09c12ab6aa0dcc83b2b2a6e3bef3d3f61
Covalentred <ayan.jayasekara@gmail.com>
2026-02-25 14:45:19 -0800

    Defined Frame Struct

---
+++ CameraParams.h
@@ -0,0 +1,13 @@
+#include <opencv2/opencv.hpp>
+#include <chrono>
+
+// Frame object holds all relevant data for each image.
+// As it passes through the pipeline, it will be filled out
+// Should start as just raw, and t
+
+struct Frame {
+  cv::Mat raw;
+  cv::Mat thresh;
+  cv::Mat trajectory_annotated;
+  std::chrono::steady_clock::time_point t;
+};
